98342392fd8b962dd25c318d693b4ab9b281d5a2
justinhornung <jhornung328i@gmail.com>
2023-01-17 19:41:31 -0500

    Product model created

--- db/migrate/20230118004014_create_products.rb
+++ db/migrate/20230118004014_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :description
 
       t.timestamps
     end
