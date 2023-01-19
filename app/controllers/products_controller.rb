2dfa2b7bc04013948edc8e73a91a48f571756dbe
justinhornung <jhornung328i@gmail.com>
2023-01-18 23:04:33 -0500

    gemfiles and configs for frontend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,35 @@ class ProductsController < ApplicationController
     render :index
   end
 
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    render :show
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
+    )
+    render :show
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "Product successfully removed from database." }
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,11 +9,36 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal Product.count, data.length
   end
 
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+    end
+  end
+
   test "show" do
     get "/products/#{Product.first.id}.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+  end
+
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+    assert_equal product.price, data["price"]
+    assert_equal product.image_url, data["image_url"]
+    assert_equal product.description, data["description"]
+  end
+
+  test "destroy" do
+    assert_difference "Product.count", -1 do
+      delete "/products/#{Product.first.id}.json"
+      assert_response 200
+    end
   end
 end
