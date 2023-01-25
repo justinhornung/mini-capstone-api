c4e4089e325eea194782cff8d9f6ac0834224e67
justinhornung <jhornung328i@gmail.com>
2023-01-24 22:33:20 -0500

    Updated test files to represent supplier_id presence

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,7 +11,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1.0, image_url: "image.jpg", description: "test description" }
+      post "/products.json", params: { supplier_id: Supplier.first.id, name: "test product", price: 1.0, image_url: "image.jpg", description: "test description" }
     end
   end
 
@@ -20,7 +20,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "image_url", "description", "created_at", "updated_at", "supplier_id"], data.keys
+    assert_equal ["id", "name", "price", "tax", "total", "is_discounted?", "image_url", "description", "supplier", "created_at", "updated_at"], data.keys
   end
 
   test "update" do
