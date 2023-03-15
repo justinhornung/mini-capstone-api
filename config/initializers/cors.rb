5364a1616e96b9efd37110319c6676104e500b6c
justinhornung <jhornung328i@gmail.com>
2023-03-14 21:33:07 -0400

    Configure cors file to use production URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:5173"
+    origins "mini-capstone.peterxjang.com", "localhost:5173", "https://keebs-r-us-37bf.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
